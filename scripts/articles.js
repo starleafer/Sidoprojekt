986bfd02e8bf166b8f7a0d8151b253fe66a0f2ad
Tobias Dedorsson <50805430+tobionesies@users.noreply.github.com>
2022-11-03 12:04:00 +0100

    generate article function

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,24 +1,12 @@
-let mainGrid = document.getElementById('main-grid');
+let mainGrid    = document.getElementById('main-grid');
 const jsSection = document.getElementById('js-generated-articles');
-const articleSection = document.createElement("section");
-
-let articles = []
+let articles    = []
 
 function Article(image, imageAlt, title, paragraph) {
-    this.image = image;
-    this.imageAlt = imageAlt;
-    this.title = title;
-    this.paragraph = paragraph;
-
-    this.articleTemplate = function() { return `
-        <section>
-        <img src="img/${this.image}" alt="${this.imageAlt}">
-        <h2>${this.title}</h2>
-        <p>${this.paragraph}</p>
-        <div id="line"></div>
-        </section>
-        `;
-    }
+    this.image      = image;
+    this.imageAlt   = imageAlt;
+    this.title      = title;
+    this.paragraph  = paragraph;
 };
 
 const addArticle = (image, imageAlt, title, paragraph) => {
@@ -28,36 +16,40 @@ const addArticle = (image, imageAlt, title, paragraph) => {
     return articles;
 }
 
-addArticle("death-metal.jpeg","image","Title", "Paragraph");
-addArticle("interview.jpeg","image","Title", "Paragraph");
-addArticle("party-hard.jpeg","image","Title", "Paragraph");
-
-console.log(articles);
-
 const createSections = (array) => {
- 
-            for(object of array) {
-                console.log(object);
-                articleSection.innerHTML = `
-                <section>
+
+    let articlesContentHtml = "";
+
+    for(object of array) {
+        console.log(object);
+        articlesContentHtml += `
+            <article>
                 <img src="img/${object.image}" alt="${object.imageAlt}">
                 <h2>${object.title}</h2>
                 <p>${object.paragraph}</p>
                 <div id="line"></div>
-                </section>
-                `;
-                jsSection.append(articleSection);
-            }
+            </article>
+        `;
+    }
+    const articleSection = document.createElement("section");
+    articleSection.innerHTML = articlesContentHtml;
+    jsSection.append(articleSection);
 }
 
-const article = {
-    image: "",
-    imageAlt: "",
-    title: "",
-    paragraph: ""
-};
+addArticle(
+    "death-metal.jpeg",
+    "Violin Violence",
+    `Death-metal bandet "Violin violence" tillbaka i Sverige på turné`,
+    "Nu är de tillbaka! Som det svenska folket har väntat på att headbanga.");
+addArticle(
+    "interview.jpeg",
+    "image",
+    "Intervju: Äldre man",
+    "Vi intervjuade Mads Epicsson, han hade inget att säga. Vi kan lära oss mycket av Mads.");
+addArticle(
+    "party-hard.jpeg",
+    "image",
+    `Agda, 98år - "Ålder stoppar oss inte från att gå cray-cray"`,
+    "Dessa damer är just kommit tillbaka från eventet Burning man, detta firas med en ordentlg förfest för att sedan gå vidare till Golden Hits.");
 
-
-
-console.log(createSections(articles));
 createSections(articles);
