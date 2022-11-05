08369a744e6ab5263502a2b5add2c517c58c6cdf
Tobias Dedorsson <50805430+tobionesies@users.noreply.github.com>
2022-11-05 11:53:06 +0100

    refactored articles.js

--- scripts/articles.js
+++ scripts/articles.js
@@ -2,6 +2,7 @@ let mainGrid    = document.getElementById('main-grid');
 const jsSection = document.getElementById('js-generated-articles');
 let articles    = []
 
+// object template for a Article
 function Article(image, imageAlt, title, paragraph) {
     this.image      = image;
     this.imageAlt   = imageAlt;
@@ -9,26 +10,51 @@ function Article(image, imageAlt, title, paragraph) {
     this.paragraph  = paragraph;
 };
 
+// function that takes 4 parameters, creates a new object with the 4 values and stores the object into the articles[] array and return that array
 const addArticle = (image, imageAlt, title, paragraph) => {
+
     const article = new Article(image, imageAlt, title, paragraph);
     articles = [article, ...articles]
 
     return articles;
 }
 
+addArticle(
+    "death-metal.jpeg",
+    "Violin Violence",
+    `Death-metal bandet "Violin violence" tillbaka i Sverige på turné`,
+    "Nu är de tillbaka! Som det svenska folket har väntat på att headbanga."
+);
+
+addArticle(
+    "interview.jpeg",
+    "image",
+    "Intervju: Äldre man",
+    "Vi intervjuade Mads Epicsson, han hade inget att säga. Vi kan lära oss mycket av Mads."
+);
+
+addArticle(
+    "party-hard.jpeg",
+    "image",
+    `Agda, 98år - "Ålder stoppar oss inte från att gå cray-cray"`,
+    "Dessa damer är just kommit tillbaka från eventet Burning man, detta firas med en ordentlg förfest för att sedan gå vidare till Golden Hits."
+);
+
+
+// function that generates the html and injects it into <main>
 const createSections = (array) => {
-
+    
     let articlesContentHtml = "";
-
+    
     for(object of array) {
         console.log(object);
         articlesContentHtml += `
-            <article>
-                <img src="img/${object.image}" alt="${object.imageAlt}">
-                <h2>${object.title}</h2>
-                <p>${object.paragraph}</p>
-                <div id="line"></div>
-            </article>
+        <article>
+        <img src="img/${object.image}" alt="${object.imageAlt}">
+        <h2>${object.title}</h2>
+        <p>${object.paragraph}</p>
+        <div id="line"></div>
+        </article>
         `;
     }
     const articleSection = document.createElement("section");
@@ -36,20 +62,5 @@ const createSections = (array) => {
     jsSection.append(articleSection);
 }
 
-addArticle(
-    "death-metal.jpeg",
-    "Violin Violence",
-    `Death-metal bandet "Violin violence" tillbaka i Sverige på turné`,
-    "Nu är de tillbaka! Som det svenska folket har väntat på att headbanga.");
-addArticle(
-    "interview.jpeg",
-    "image",
-    "Intervju: Äldre man",
-    "Vi intervjuade Mads Epicsson, han hade inget att säga. Vi kan lära oss mycket av Mads.");
-addArticle(
-    "party-hard.jpeg",
-    "image",
-    `Agda, 98år - "Ålder stoppar oss inte från att gå cray-cray"`,
-    "Dessa damer är just kommit tillbaka från eventet Burning man, detta firas med en ordentlg förfest för att sedan gå vidare till Golden Hits.");
-
+// Calls the function createSections()
 createSections(articles);
