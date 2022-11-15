867ee4075d1eed090b03e880170ac200e3e2e868
Tobias Dedorsson <50805430+tobionesies@users.noreply.github.com>
2022-11-15 08:55:35 +0100

    shark-function

--- scripts/fish-game.js
+++ scripts/fish-game.js
@@ -2,16 +2,17 @@ function linkFishGame() {
 
     let startButton = document.getElementById("start-btn-fish");
 
-    startButton.addEventListener("click", function() {
-        startButton.remove();
 
-        window.addEventListener("keydown", function(event) {
+    startButton.addEventListener("click", function () {
+        startButton.remove();
+        sharkFunction();
+
+        window.addEventListener("keydown", function (event) {
             event.preventDefault();
-            switch(event.key) {
-                case "ArrowUp": 
-                    console.log("upp");
+            switch (event.key) {
+                case "ArrowUp":
                     moveUp();
-                    break; 
+                    break;
                 case "ArrowDown":
                     moveDown();
                     break;
@@ -21,11 +22,53 @@ function linkFishGame() {
 
     function moveUp() {
         let fish = document.getElementById("fish-img"),
-        style = window.getComputedStyle(fish),
-        top = style.getPropertyValue("top");
-        console.log(top.slice(0, -2));
+            style = window.getComputedStyle(fish),
+            top = style.getPropertyValue("top");
+        let topPosition = parseInt(top.slice(0, -2));
+        if (topPosition > 20) {
+            topPosition -= 10;
+        }
+        fish.style.top = `${topPosition}px`;
 
     }
+    function moveDown() {
+        let fish = document.getElementById("fish-img"),
+            style = window.getComputedStyle(fish),
+            top = style.getPropertyValue("top");
+        let topPosition = parseInt(top.slice(0, -2));
+        if (topPosition < 480) {
+            topPosition += 10;
+        }
+        fish.style.top = `${topPosition}px`;
+    }
+    function sharkFunction() {
+
+        let shark = document.createElement("img");
+        shark.setAttribute("src", "img/shark.png");
+        shark.setAttribute("id", "shark-img")
+
+        document.getElementById("fish-background").append(shark);
+
+
+
+    }
+
+    function checkIfDead() {
+        let fishImg = document.getElementById("fish-img");
+        let sharkImg = document.getElementById("shark-img");
+
+        var fish = parseInt(window.getComputedStyle(fishImg).getPropertyValue("top"));
+        var shark = parseInt(window.getComputedStyle(sharkImg).getPropertyValue("left"));
+        console.log(fish);
+        console.log(shark);
+        if (fish > 400 && fish < 450 && shark < 100 && shark > -100) {
+            alert("you dead");
+        }
+    }
+
+    setInterval(checkIfDead, 100);
+
+    setInterval(sharkFunction, 5000);
 
 
 
