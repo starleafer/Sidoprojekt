ff23e1fd9ee08ac74f20fbcc44e0e225d3bd12fc
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-01-02 15:54:52 +0100

    small changes

--- scripts/board-game.js
+++ scripts/board-game.js
@@ -1,15 +1,20 @@
 function linkBoardGame() {
+
     let playerPosition = 0;
+    let enemyPosition = 0;
+    let counter = 0;
     let chancePositions = [5, 7, 9, 11, 14, 17, 18];
     let chanceEvent =["Hoppa fram två steg", "Stå över ett slag", "Gå ett steg fram"];
     let chanceBtn = document.getElementById("chance-cards");
 
     document.getElementById("dice").addEventListener("click", (e) => {
+        counter++;
         e.target.classList.add("big-text");
         let diceThrow = Math.floor(Math.random() * 2) + 1;
         e.target.innerText = diceThrow;
         color(diceThrow);
         checkForChance(playerPosition);
+        enemyMove();
     })
 
     chanceBtn.addEventListener("click", () => {
@@ -30,6 +35,17 @@ function linkBoardGame() {
 
         setTimeout(setDisabled, 1000);
     })
+
+    function enemyMove() {
+        if (counter > 2) {
+            let enemySteps = Math.floor(Math.random() * 3) + 1;
+            enemyPosition = enemyPosition + enemySteps;
+            document.getElementById(`${enemyPosition}`).classList.add("enemy");
+            if (enemyPosition >= playerPosition) {
+                alert("Du förlorade!")
+            }
+        }
+    }
 
     function color(steps) {
         for (i = 0; i < steps; i++) {
