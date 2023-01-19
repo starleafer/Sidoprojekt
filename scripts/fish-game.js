ebfedb7bd84629e8138d0ff154eab1e6c89ea65d
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2023-01-19 17:34:29 +0100

    added some more background elements

--- scripts/fish-game.js
+++ scripts/fish-game.js
@@ -13,6 +13,8 @@ function linkFishGame() {
     
         const fish_backround = new Image();
         fish_backround.src = "img/fish-game/background.png";
+        const fish_backround2 = new Image();
+        fish_backround2.src = "img/fish-game/transparent-background.png";
         const fish_school1 = new Image();
         fish_school1.src = "img/fish-game/yellow-fishschool.png";
         const fish_school2 = new Image();
@@ -21,6 +23,16 @@ function linkFishGame() {
         fish_waves.src = "img/fish-game/waves.png";
         const fish_sand = new Image();
         fish_sand.src = "img/fish-game/sand.png";
+        const coral1 = new Image();
+        coral1.src ="img/fish-game/coral1.png";
+        const coral2 = new Image();
+        coral2.src ="img/fish-game/coral2.png";
+        const coral3 = new Image();
+        coral3.src ="img/fish-game/coral3.png";
+        const coral4 = new Image();
+        coral4.src ="img/fish-game/coral3.png";
+
+
 
         let x = 0;
         let x2 = 800;
@@ -28,14 +40,23 @@ function linkFishGame() {
         function animate() {
             ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
             ctx.drawImage(fish_backround, 0, 0)
-            ctx.drawImage(fish_school1, x, 0)
+            // ctx.drawImage(coral2, x, 0)
             ctx.drawImage(fish_school2, x, 0)
-
+            ctx.drawImage(coral1, x, 0)
+            ctx.drawImage(coral3, x, 0)
+            ctx.drawImage(coral1, x2, 0)
+            ctx.drawImage(coral3, x2, 0)
+            ctx.drawImage(fish_school1, x, 0)
+            ctx.drawImage(fish_backround2, 0, 0)
+            
+            
             ctx.drawImage(fish_sand, x, 0)
             ctx.drawImage(fish_waves, x, 0)
             ctx.drawImage(fish_sand, x2, 0)
             ctx.drawImage(fish_waves, x2, 0)
-
+            
+            // ctx.drawImage(coral4, x, 0)
+            
             if (x < -800) {
                 x = 799 +x2;
             } else {
